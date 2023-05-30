7207a66ba7af0e3801b67b563b43dd55ca9b5798
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-05-30 10:06:27 +0700

    feat: migrations and seeders file

---
+++ database/seeders/MovieTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Movie;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MovieTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $movies = [
+            [
+                'name' => 'Avatar 2',
+                'slug' => 'avatar-2',
+                'category' => 'Action',
+                'video_url' => 'https://youtube.com/eo12ihwhw9r82yrew',
+                'thumbnail' => 'https://picsum.photos/id/3/300/500',
+                'rating' => 8.1,
+                'is_featured' => 1
+            ], [
+                'name' => 'Knives Out',
+                'slug' => 'knives-out',
+                'category' => 'Mystery',
+                'video_url' => 'https://youtube.com/eo12ihwhw9r82yrew',
+                'thumbnail' => 'https://picsum.photos/id/4/300/500',
+                'rating' => 8.8,
+                'is_featured' => 0
+            ], [
+                'name' => 'One Piece',
+                'slug' => 'one-piece',
+                'category' => 'Animation',
+                'video_url' => 'https://youtube.com/eo12ihwhw9r82yrew',
+                'thumbnail' => 'https://picsum.photos/id/4/300/500',
+                'rating' => 8.1,
+                'is_featured' => 0
+            ]
+        ];
+
+        Movie::insert($movies);
+    }
+}
